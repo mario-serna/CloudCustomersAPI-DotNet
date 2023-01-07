81a2f5d9c635ab1ca8ca1523acac68994a82e85a
Mario Serna <mario.serna@softtek.com>
2023-01-07 15:26:39 -0600

    add User and Address Models

---
+++ CloudCustomers.API/Models/Address.cs
@@ -0,0 +1,8 @@
+namespace CloudCustomers.API.Models;
+
+public class Address
+{
+    public string? City { get; set; }
+    public string? State { get; set; }
+    public int ZipCode { get; set; }
+}
---
+++ CloudCustomers.API/Models/User.cs
@@ -0,0 +1,9 @@
+namespace CloudCustomers.API.Models;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public Address? Address { get; set; }
+}
